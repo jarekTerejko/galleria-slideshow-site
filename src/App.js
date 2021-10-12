475333b209b1315f4eb19a7d90e8b3eb4c342384
jarekTerejko <j.terejko@gmail.com>
2021-10-12 05:19:06 +0200

    import Context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useContext } from "react";
+import Gallery from "./components/Gallery";
+import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
+import { AppContext } from "./context/AppContext";
 function App() {
+  const {
+    isLoading,
+    startSlideShow,
+    isSlideShowOn,
+    currentSlide,
+    data,
+    stopSlideShow,
+  } = useContext(AppContext);
+
   return (
     <>
-    App
+      <Navbar
+        isSlideShowOn={isSlideShowOn}
+        startSlideShow={startSlideShow}
+        stopSlideShow={stopSlideShow}
+      />
+      {isLoading ? <Loader /> : null}
+      {data.length === 0 ? null : (
+        <Gallery
+          data={data}
+          startSlideShow={startSlideShow}
+          isSlideShowOn={isSlideShowOn}
+          currentSlide={currentSlide}
+        />
+      )}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppContextProvider from './context/AppContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
